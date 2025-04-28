4b492e71f184fdf0f00d833e52431a58950fbae9
DingMie <ding_mie@163.com>
2025-04-28 21:34:20 +0800

    写完剩下的逻辑

--- JinChanChan/config.py
+++ JinChanChan/config.py
@@ -1,6 +1,7 @@
 
 class Config:
-    pictrue_dir=r'D:\D_disk\DATA\project\python_project\CV\JinChanChan_github\JinChanChan\pictures'
+    pictrue_dir=r'D:\D_disk\BaiduSyncdisk\project\python_project\CV\JinChanChan_github\JinChanChan\pictures'
+    lineup_dir=r'D:\D_disk\BaiduSyncdisk\project\python_project\CV\JinChanChan_github\JinChanChan\阵容'
     output_idx=1
     point= -1392,897
     h=167
--- JinChanChan/utils.py
+++ JinChanChan/utils.py
@@ -1,5 +1,10 @@
 import os
+
+import pyautogui
 from PIL import Image
+
+from config import Config
+
 
 def load_imgs(folder_path):
     imgs = {}
@@ -14,3 +19,41 @@ def load_imgs(folder_path):
             else:
                 print(f"警告：无法加载图片 {filename}")
     return imgs
+
+def get_heros(lineup_dir):
+    # 读取文件内文件名
+    filenames=[]
+    for i,filename in enumerate(os.listdir(lineup_dir)):
+        print(i,":",filename)
+        filenames.append(filename)
+    print('---------请输入阵容序号---------')
+    idx = input('请输入选择阵容序号：')
+    filename=filenames[int(idx)]
+    heros_path=os.path.join(lineup_dir,filename)
+    with open(heros_path, 'r', encoding='utf-8') as file:
+        heros = [line.strip() for line in file]
+    return heros,filename
+
+def get_imgs(grab):
+    # 得到当前5张图片
+    img=None
+    while img is None:
+        img = grab.grab()
+    # print('截图成功')
+    sub_imgs = []
+    x, y = Config.point
+    for _ in range(5):
+        sub_imgs.append(img[y:y + Config.h,
+                        x:x + Config.w, :])
+        x += Config.move
+    return sub_imgs
+
+def click(idx,name):
+    x,y=Config.point
+    pyautogui.moveTo(x+50+idx*Config.move, y+50, duration=0)
+    for _ in range(2):
+        pyautogui.mouseDown( button="left")
+        # time.sleep(0.01)
+        pyautogui.mouseUp(button="left")
+        # time.sleep(0.01)
+    print(f'\033[32m点击图片{idx + 1}{name}!\033[0m')
