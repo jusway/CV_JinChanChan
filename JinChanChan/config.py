07a7b6775496725a2091431090d41f473d640d72
DingMie <ding_mie@163.com>
2025-05-12 08:09:51 +0800

    添加新的阵容和修改

--- JinChanChan/config.py
+++ JinChanChan/config.py
@@ -1,9 +1,10 @@
 
 class Config:
-    pictrue_dir=r'D:\D_disk\BaiduSyncdisk\project\python_project\CV\JinChanChan_github\JinChanChan\pictures'
-    lineup_dir=r'D:\D_disk\BaiduSyncdisk\project\python_project\CV\JinChanChan_github\JinChanChan\阵容'
-    output_idx=1
-    point= -1392,897
-    h=167
+    pictrue_dir=r'D:\D_disk\BaiduSyncdisk\project\python_project\CV\CV_JinChanChan\JinChanChan\pictures'
+    lineup_dir=r'D:\D_disk\BaiduSyncdisk\project\python_project\CV\CV_JinChanChan\JinChanChan\阵容'
+    output_idx=0
+    point= -1377,900
+    h=164
     w=205
-    move=216
+    move=207
+    y_bias=0
--- JinChanChan/截图.py
+++ JinChanChan/截图.py
@@ -2,19 +2,12 @@ import dxcam
 from config import Config
 from PIL import Image
 
+from utils import get_imgs
+
+
 def main():
-    grab = dxcam.create(output_idx=Config.output_idx, output_color='RGB')
-    img=None
-    while img is  None:
-        img = grab.grab()
-    # print(type(img))
-    # print(img.shape)
-    sub_imgs=[]
-    x,y=Config.point
-    for _ in range(5):
-        sub_img=img[y:y + Config.h  ,x:x + Config.w,  :]
-        sub_imgs.append(sub_img)
-        x+=Config.move
+    grab=dxcam.create(output_idx=Config.output_idx,output_color='RGB')
+    sub_imgs=get_imgs(grab)
 
     # 保存
     for i,sub_img in enumerate(sub_imgs):
